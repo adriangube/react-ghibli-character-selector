ce2269cc63e9305380f3205b233d124f7da62710
adriangube <adrianguillenbermudez@gmail.com>
2022-08-20 18:42:51 +0200

    add alt and height in images in banner component

--- src/ui/components/Banner/Banner.tsx
+++ src/ui/components/Banner/Banner.tsx
@@ -15,17 +15,20 @@ export const Banner = ({ imageUrl }:BannerProps ): JSX.Element => {
 					onError={(e) => {
 						e.currentTarget.src = fallbackImage;
 						e.currentTarget.classList.add('Banner__image--error');
-					}} 
+					}}
 					width="100%"
+					height="200px"
 					className='Banner__image'
 					loading='lazy'
+					alt="banner"
 				/>
 			)}
 			{!imageUrl && (
 				<img
 					src={fallbackImage}
-					alt="fallback"
+					alt="fallback banner"
 					width="100%"
+					height="200px"
 					className='Banner__fallback'
 					loading='lazy'
 				/>
