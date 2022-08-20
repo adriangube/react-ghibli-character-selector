9d02b9977a2e78a0dc33147d6526e688090db8bd
adriangube <adrianguillenbermudez@gmail.com>
2022-08-20 11:19:58 +0200

    add test in click handler

--- src/ui/components/FilmItem/FilmItem.spec.tsx
+++ src/ui/components/FilmItem/FilmItem.spec.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import {FilmItem} from './FilmItem';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import {FILM_MOCK} from './FilmItemMockData';
 import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 const FILM_ITEM_TEST_ID = 'FilmItem';
 const IMAGE_TEST_ID = 'FilmItem__image';
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+	...jest.requireActual('react-router-dom'),
+	useNavigate: () => mockedUsedNavigate,
+}));
+
 describe('FilmItem', () => {
-	it('should be in the document with the data provided', () => {
-		render(<FilmItem film={FILM_MOCK} />);
+	it('should be in the document with the data provided', async() => {
+		render((
+			<MemoryRouter initialEntries={['/films']}>
+				<FilmItem film={FILM_MOCK} />
+			</MemoryRouter>
+		));
 		const filmItem = screen.getByTestId(FILM_ITEM_TEST_ID);
 		const image: HTMLImageElement = screen.getByTestId(IMAGE_TEST_ID);
 		const title = screen.getByText(FILM_MOCK.title);
@@ -18,5 +30,11 @@ describe('FilmItem', () => {
 		expect(title).toBeInTheDocument();
 		expect(image.src).toStrictEqual(FILM_MOCK.movieBanner);
 		expect(image.alt).toStrictEqual(`${FILM_MOCK.title} banner`);
+		expect(mockedUsedNavigate).toHaveBeenCalledTimes(0);
+		fireEvent.click(filmItem);
+		await waitFor(() => {
+			expect(mockedUsedNavigate).toHaveBeenCalledTimes(1);
+			expect(mockedUsedNavigate).toHaveBeenCalledWith(`/films/${FILM_MOCK.id}`);
+		});
 	});
 });
--- src/ui/components/FilmItem/FilmItem.tsx
+++ src/ui/components/FilmItem/FilmItem.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import {Film} from '../../../domain/film/film';
 import './FilmItem.css';
 
@@ -6,9 +7,14 @@ export type FilmItemProps = {
     film: Film;
 }
 
-export const FilmItem = ({ film }:FilmItemProps ): JSX.Element => {
+export const FilmItem = ({film}: FilmItemProps): JSX.Element => {
+	const navigate = useNavigate();
+	const onClickHandler = () => {
+		navigate(`/films/${film.id}`);
+	};
+
 	return (
-		<div className="FilmItem" data-testid="FilmItem">
+		<div className="FilmItem" data-testid="FilmItem" onClick={onClickHandler}>
 			<img
 				src={film.movieBanner}
 				alt={`${film.title} banner`}
