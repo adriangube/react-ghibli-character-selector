8a4624ee63672d18cf0777ba3b1986cd35ceb9ab
adriangube <adrianguillenbermudez@gmail.com>
2022-08-22 18:44:56 +0200

    add lazy loading in images and remove maskable logos in manifest

--- src/ui/components/FilmItem/FilmItem.tsx
+++ src/ui/components/FilmItem/FilmItem.tsx
@@ -22,6 +22,7 @@ export const FilmItem = ({film}: FilmItemProps): JSX.Element => {
 				alt={`${film.title} banner`}
 				className="FilmItem__image"
 				data-testid="FilmItem__image"
+				loading='lazy'
 			/>
 			<div className='FilmItem__title'>{film.title}</div>
 		</div>
